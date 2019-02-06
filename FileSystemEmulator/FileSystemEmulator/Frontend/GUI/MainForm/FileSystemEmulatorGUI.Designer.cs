3e60e0b2800f876536b617879728c1067ad79195
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-02-06 09:35:03 +0100

    Non relevant changes

--- FileSystemEmulator/FileSystemEmulator/Frontend/GUI/MainForm/FileSystemEmulatorGUI.Designer.cs
+++ FileSystemEmulator/FileSystemEmulator/Frontend/GUI/MainForm/FileSystemEmulatorGUI.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.listDirectory = new System.Windows.Forms.ListBox();
             this.splitContMain = new System.Windows.Forms.SplitContainer();
-            this.btnSuperDir = new System.Windows.Forms.Button();
-            this.txtPath = new System.Windows.Forms.TextBox();
             this.btnGo = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnSuperDir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContMain)).BeginInit();
             this.splitContMain.Panel1.SuspendLayout();
             this.splitContMain.Panel2.SuspendLayout();
@@ -71,27 +71,6 @@
             this.splitContMain.SplitterDistance = 29;
             this.splitContMain.TabIndex = 1;
             // 
-            // btnSuperDir
-            // 
-            this.btnSuperDir.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnSuperDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSuperDir.Location = new System.Drawing.Point(0, 0);
-            this.btnSuperDir.Name = "btnSuperDir";
-            this.btnSuperDir.Size = new System.Drawing.Size(75, 29);
-            this.btnSuperDir.TabIndex = 0;
-            this.btnSuperDir.Text = "<";
-            this.btnSuperDir.UseVisualStyleBackColor = true;
-            this.btnSuperDir.Click += new System.EventHandler(this.btnSuperDir_Click);
-            // 
-            // txtPath
-            // 
-            this.txtPath.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPath.Location = new System.Drawing.Point(75, 0);
-            this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(725, 28);
-            this.txtPath.TabIndex = 1;
-            // 
             // btnGo
             // 
             this.btnGo.Dock = System.Windows.Forms.DockStyle.Right;
@@ -103,6 +82,27 @@
             this.btnGo.Text = "GO";
             this.btnGo.UseVisualStyleBackColor = true;
             this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPath.Location = new System.Drawing.Point(75, 0);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(725, 28);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnSuperDir
+            // 
+            this.btnSuperDir.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSuperDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuperDir.Location = new System.Drawing.Point(0, 0);
+            this.btnSuperDir.Name = "btnSuperDir";
+            this.btnSuperDir.Size = new System.Drawing.Size(75, 29);
+            this.btnSuperDir.TabIndex = 0;
+            this.btnSuperDir.Text = "<";
+            this.btnSuperDir.UseVisualStyleBackColor = true;
+            this.btnSuperDir.Click += new System.EventHandler(this.btnSuperDir_Click);
             // 
             // FileSystemEmulatorGUI
             // 
